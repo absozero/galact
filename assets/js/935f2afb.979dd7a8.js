4d0968e67a768e70dde89c6c2378d79e9a5d1875
Absozero <aswsub123@gmail.com>
2021-11-14 01:23:28 -0800

    Deploy website - based on fec2d5618f4ba9154cbf2511a0ca3cc6f06c9b26

--- assets/js/935f2afb.979dd7a8.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkgalact_docs=self.webpackChunkgalact_docs||[]).push([[53],{1109:function(e){e.exports=JSON.parse('{"pluginId":"default","version":"current","label":"Next","banner":null,"badge":false,"className":"docs-version-current","isLast":true,"docsSidebars":{"tutorialSidebar":[{"type":"link","label":"Tutorial Intro","href":"/Galact/docs/intro"}]}}')}}]);

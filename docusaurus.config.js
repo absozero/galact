555b97000ae784ad03ad4b0c8c1d3ca0f99e50bc
Aswath Subramanian <aswsub123@gmail.com>
2021-12-04 15:20:55 -0800

    remove

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
+          
           editUrl: 'https://github.com/absozero/Galact/tree/documentation/',
         },
         blog: {

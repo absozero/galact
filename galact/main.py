3512bedd3086b70ed06bcbe91e5c50cb7686a424
Gitea <gitea@fake.local>
2022-04-07 23:27:19 -0700

    make galact stylish and modern

--- galact/main.py
+++ galact/main.py
@@ -1,14 +1,19 @@
 import re
+import time
 import random
 import argparse
 import sys
 import signal
 import rich
 from rich.console import Console
+from rich.progress import track
 
 console = Console()
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(prog='galact',
+        usage='%(prog)s (subcommands) [options]',
+        epilog="Have fun with the chatbot!",
+        description='Galact is a command line ai that can be used to interact with the user in a more natural way.')
 subparsers = parser.add_subparsers()
 
 RESPONSES = {
@@ -130,6 +135,7 @@ class Galact():
         
         console.print('Hello!:wave:, I am [bold cyan]Galact[/bold cyan]')
         console.print('[underline]A bot[/] made by [green]Abso[/][blue]zero[/], using re.')
+        console.print('Make sure to have fun!', style="bold underline blue")
         console.print('enter [underline][bold red]\'quit\' or \'exit\'[/underline], [/bold red] to exit the program')
 
         self.run()
@@ -147,22 +153,36 @@ class Galact():
     def run(self):
         while True:
 
-            texts = console.input('[italic purple]Send message: ')
+            texts = console.input('[italic blue_violet on bright_white]Send message:[/] ')
             
             if texts == "code":
-                console.print("[bold italic link=https://github.com/absozero/Galact]https://github.com/absozero/galact :man_technologist_medium_skin_tone:")
+                n = 170
+                for n in track(range(n), description="[bold light_pink3]Scraping the web for the repository..."):
+                    time.sleep(0.01)
+                console.print("[bold italic link=https://github.com/absozero/Galact]https://github.com/absozero/galact[/]")
+                console.print("[chartreuse3]Do not use this text or the emoji as the link :man_technologist_medium_skin_tone:")
 
-            if texts.lower() in quit_text:
-                sys.exit(console.print('[bold red]Exited Galact with the appropriate phrase'))
-
+            elif texts.lower() in quit_text:
+                try:
+                    n = 120
+                    for n in track(range(n), description="[bold red]Shutting down all processes of the ai..."):       
+                        time.sleep(0.013)
+                    sys.exit(console.print('[bold red]Exited Galact with the appropriate phrase'))
+                except KeyboardInterrupt:
+                    sys.exit(console.print('[bold red]Exited Galact with the appropriate phrase'))
+            
             else:
-                console.print('[bold green]Galact says:', self.match(texts))
+                n = 120
+                for n in track(range(n), description="[bold light_goldenrod2]Searching database for viable replies..."):
+                    time.sleep(0.01)
+                console.print('[bold medium_spring_green on dark_blue]Galact says:', self.match(texts))
 
 
 def about():
-    console.print('''[bold light cyan]
-        This project was made in order to put an ai, a speaking one, into the command line using regular expressions, and since this project had ideas from all sides of the galaxy, the project is called Galact.
-            ''')
+    n = 110
+    for n in track(range(n), description="[bold cyan]Grabbing info about bot..."):
+        time.sleep(0.007)
+    console.print('[underline][bold blue]This project was made in order to put an ai, a speaking one, into the command line using regular expressions, and since this project had ideas from all sides of the galaxy, the project is called Galact.')
 
 parser_galact = subparsers.add_parser('run', help='Run galact on cli')
 parser_galact.set_defaults(func=Galact)
@@ -180,10 +200,28 @@ if len(sys.argv) <= 1:
 
 def main():
     try:
-        gal = parser.parse_args()
-        gal.func()
+        n = 133 
+        for n in track(range(n), description="[bold green]Starting all processes of the ai..."):
+            time.sleep(0.013)
+        try:
+            gal = parser.parse_args()
+            gal.func()
+        except KeyboardInterrupt:
+            try:
+                n = 130
+                print('\n')
+                for n in track(range(n), description="[bold red]Shutting down all processes of the ai..."):
+                    time.sleep(0.013)
+                console.print('[bold red]Exited Galact with the appropriate key combo')
+            except KeyboardInterrupt:
+                console.print('[bold red]Exited Galact with the appropriate key combo')
+        except EOFError:
+            n=130
+            for n in track(range(n), description="[bold red]Shutting down all processes of the ai..."):
+                time.sleep(0.013)
+            console.print('\n[bold red]Exited Galact with the EOF key combo')
     except KeyboardInterrupt:
-        console.print('[bold red]Exited Galact with the appropriate phrase')
+        console.print('\n[bold red]Exited Galact with the appropriate key combo')
 '''
 def sigint_handler(signal, frame):
     console.print('[bold red]Exited Galact with key combo')
